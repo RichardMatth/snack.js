fa7912b3e38a35f5e187f6c7e213843c52dc2088
Richard Matheus <107589526+RichardMatth@users.noreply.github.com>
2024-02-10 17:14:25 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoeaded', () => {
+    const square = document.querySelectorAll ('.grid Div')
+    const scoreDisplay = document.querySelector ('.span')
+    const startBtn = document.querySelector ('start')
+
+    const width = 10
+    let currentIndex = 0   // so firs div in our grid
+    let appleIndex = 0
+    let currentSnake = [2, 1, 0]
+
+    let direction: 1
+    let score: 0
+    let speed: 1.0
+    let intervalTime = 0
+    let interval = 0
+
+})
