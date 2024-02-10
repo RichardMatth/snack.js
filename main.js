a87f0536b3c386d31d3893bbee07fe3a352e1377
Richard Matheus <107589526+RichardMatth@users.noreply.github.com>
2024-02-10 18:43:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -34,13 +34,18 @@ document.addEventListener('DOMContentLoeaded', () => {
     function moveOutcomes() {
     
     // snake hitting self
-    if {
+    if (
         (currentSnake[0] + width >= (width * width) && direction === width) || //if snake hits bottom
         (currentSnake[0] % width === width -1 && direction === 1) ||
-        (currentSnake[0] % width === 0 && direction === -1)
-        (currentSnake[0] - width < 0 && direction === -width)
+        (currentSnake[0] % width === 0 && direction === -1) ||
+        (currentSnake[0] - width < 0 && direction === -width) ||
+        squares.[currentSnake[0] + direction].classList.contains('snakes')
 
-    }
+   ) {
+    return clearInterval(interval)
+   }
+
+
 
     // assign functions to keycodes
     function control(e) {
